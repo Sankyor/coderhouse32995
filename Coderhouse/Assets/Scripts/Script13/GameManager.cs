8a534f35d9875088a90474849ce93e5f103eaebc
Sanky <nicolas.calvo.koch@gmail.com>
2023-02-22 16:58:24 -0300

    Acomodacion y actualizacion del proyecto.

---
+++ Coderhouse/Assets/Scripts/Script13/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Contara cuantos switches logra hacer el jugador y cuanto es el record actual
+
+
+}
